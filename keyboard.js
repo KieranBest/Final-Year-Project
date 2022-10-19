0b4e5e7c7baf9727cfea11419a4b9ba9a66e13b0
Kieran Best <74361879+KieranBest@users.noreply.github.com>
2022-10-19 13:30:18 +0100

    Created and linked files, HTML canvas, MIDI Input
    Started working on HTML Canvas and understanding how I can use this to create a keyboard. Moved on to getting MIDI keyboard so then the logical step would be to apply the MIDI Input to make a change happen in the canvas.

---
+++ keyboard.js
@@ -0,0 +1,59 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+let cw = canvas.width=window.innerWidth;
+let ch = canvas.height=window.innerHeight/2;
+
+
+c.fillRect(cw,ch,10,10);
+c.fillRect(30,30,10,10);
+c.fillRect(100,100,10,10);
+
+
+if(navigator.requestMIDIAccess){
+    navigator.requestMIDIAccess().then(success, failure);
+}
+
+function success(midiAccess){
+    midiAccess.addEventListener('statechange',updateDevices);
+
+    const inputs = midiAccess.inputs;
+
+    inputs.forEach((input) => {
+        input.addEventListener('midimessage', handleInput)
+    })
+}
+
+function handleInput(input){
+    const command = input.data[0];
+    const note = input.data[1];
+    const velocity = input.data[2];
+
+    switch(command){
+        case 144: //noteOn
+        if (velocity>0){
+            noteOn(note,velocity);
+        }else {
+            noteOff(note);
+        }
+        break;
+        case 128: //noteOff
+        noteOff(note);
+    }
+}
+
+function noteOn(note,velocity){
+    console.log(note,velocity);
+}
+
+function noteOff(note){
+    console.log(note);
+}
+
+function updateDevices(event){
+    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State:${event.port.state}, Type:${event.port.type}`)
+}
+
+function failure(){
+    console.log('Could not connect MIDI')
+}
