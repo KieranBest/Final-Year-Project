2c3b1f8003350eb32993ac551da70f03f24b9f28
Kieran Best <74361879+KieranBest@users.noreply.github.com>
2022-10-26 11:45:11 +0100

    Successfully displays what key is being pressed
    Displays key pressed on the terminal.

--- keyboard.js
+++ keyboard.js
@@ -1,5 +1,5 @@
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
 let cw = canvas.width=window.innerWidth;
 let ch = canvas.height=window.innerHeight/2;
@@ -9,11 +9,11 @@ c.fillRect(200,200,50,50);
 c.fillRect(30,30,10,10);
 c.fillRect(100,100,10,10);
 
-
+//When the Midi device is first connected it will 
+//display device features if successful
 if(navigator.requestMIDIAccess){
     navigator.requestMIDIAccess().then(success, failure);
 }
-
 function success(midiAccess){
     midiAccess.addEventListener('statechange',updateDevices);
 
@@ -23,7 +23,14 @@ function success(midiAccess){
         input.addEventListener('midimessage', handleInput)
     })
 }
+function failure(){
+    console.log('Could not connect MIDI')
+}
+function updateDevices(event){
+    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State:${event.port.state}, Type:${event.port.type}`)
+}
 
+//Input data to define what to do when key pressed
 function handleInput(input){
     const command = input.data[0];
     const note = input.data[1];
@@ -41,26 +48,23 @@ function handleInput(input){
         noteOff(note);
     }
 }
-
 function noteOn(note,velocity){
     console.log(note,velocity);
     c.fillStyle="#cc0000";
     c.fillRect(30,30,10,10);
-    c.fill
+    c.fill;
+    console.log(noteNumber(note));
 }
-
-function noteOff(note){
+function noteOff(note){    
+    console.log(note);
     c.fillStyle="#000000";
     c.fillRect(30,30,10,10);
-    c.fill
-
-    console.log(note);
+    c.fill;
 }
 
-function updateDevices(event){
-    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State:${event.port.state}, Type:${event.port.type}`)
-}
-
-function failure(){
-    console.log('Could not connect MIDI')
+//Input data taken to define which key pressed
+var noteLetter = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+function noteNumber(note){
+    var noteNumber = note % 12;
+    return noteLetter[noteNumber];
 }
