94e174bb42cf91e574cd5b68ddadba3d35512e44
Kieran Best <74361879+KieranBest@users.noreply.github.com>
2022-11-09 14:52:22 +0000

    Created a dynamic music sheet layout
    Created basic dynamic music sheet layout as well as tidied up other parts and altered sizing of keyboard to ensure comfortable size for all viewing screens.

--- script.js
+++ script.js
@@ -83,50 +83,82 @@ function findNoteLetter(note){
     return noteLetter[noteNumber];
 }
 
-
+// Window starting gets the keyboard statistics needed before creating keyboard
 window.onload = function(){
-    keyboardStats();
+    canvasStats();
+    drawSheet();
     drawKeyboard();
 }
 
+// Window resizing gets the screen and new keyboard statistics needed before creating keyboard
 window.onresize = function(){
-    keyboardStats();
+    canvasStats();
+    drawSheet();
     drawKeyboard();
 }
+// https://www.youtube.com/watch?v=uq66IuqYdWg
 
 // Canvas creation
 var canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-
-
-function keyboardStats(){
-    cw = canvas.width = window.innerWidth*0.99;
-    ch = canvas.height = window.innerHeight/2;
-    whiteKeyWidth = ((cw-10)/numWhiteKeys);
-    whiteKeyHeight = ch;
-    blackKeyWidth = whiteKeyWidth*0.5;
-    blackKeyHeight = ch*0.66;
-    xCoordinate = cw/numWhiteKeys;
-}
-
 // Midi keyboard key information
 var totalKeys = 32    //ready for input from midi device, once this is done, possibly move into a function
 var numWhiteKeys = 19;
 
-// Canvas Keyboard Drawing
+function canvasStats(){
+    cw = canvas.width = window.innerWidth*0.99;
+    ch = canvas.height = (window.innerHeight/4)*3;
+    whiteKeyWidth = ((cw-10)/numWhiteKeys);
+    whiteKeyHeight = (ch/7)*4;
+    blackKeyWidth = whiteKeyWidth*0.5;
+    blackKeyHeight = whiteKeyHeight*0.6;
+    xCoordinate = cw/numWhiteKeys;
+    yCordinate = (ch/7)*3-10;
+    sheetSpacing = (ch/7)/6;
+    hitNoteLine = cw*0.95
+    downLinedistance = sheetSpacing*14
+}
+
+// Draws the sheet music lines
+function drawSheet(){
+    for (let i = 1; i < 6; i++){
+        ctx.beginPath();
+        ctx.moveTo(0, i*sheetSpacing);
+        ctx.lineTo(cw, i*sheetSpacing);
+        ctx.stroke();    
+    }
+    for (let i = 9; i < 14; i++){
+        ctx.beginPath();
+        ctx.moveTo(0, i*sheetSpacing);
+        ctx.lineTo(cw, i*sheetSpacing);
+        ctx.stroke();    
+    }
+    ctx.beginPath();
+    ctx.moveTo(hitNoteLine, 0);
+    ctx.lineTo(hitNoteLine, downLinedistance);
+    ctx.stroke(); 
+}
+
 function drawKeyboard(sharpNote){
-    for (let i =0;i<numWhiteKeys;i++){
+    // Border around the keyboard
+    ctx.beginPath();
+    ctx.lineWidth = "5";
+    ctx.strokeStyle = "black";
+    ctx.rect(0, yCordinate, cw, whiteKeyHeight);
+    ctx.stroke();
+
+    for (let i = 0; i < numWhiteKeys; i++){
         ctx.beginPath();
         ctx.lineWidth = "1";
         ctx.strokeStyle = "black";
-        ctx.rect(i*xCoordinate, 0, whiteKeyWidth, whiteKeyHeight);
+        ctx.rect(i*xCoordinate, yCordinate, whiteKeyWidth, whiteKeyHeight);
         ctx.stroke();
     }
     var position; // Compares i to sharpNote and if matches puts it in a new variable called position
-    for (let i =0;i<numWhiteKeys-1;i++){                
-        for(sharpNotePosition in sharpNote){
-            if(sharpNote[sharpNotePosition] == i){
+    for (let i = 0; i < numWhiteKeys - 1; i++){                
+        for (sharpNotePosition in sharpNote){
+            if (sharpNote[sharpNotePosition] == i){
                 position = i
             }
         }
@@ -135,14 +167,14 @@ function drawKeyboard(sharpNote){
             ctx.beginPath();
             ctx.fillStyle="black";
             ctx.lineWidth = "1";
-            ctx.rect((i+0.75)*xCoordinate, 0, blackKeyWidth, blackKeyHeight);
+            ctx.rect((i+0.75)*xCoordinate, yCordinate, blackKeyWidth, blackKeyHeight);
             ctx.fill();            
         }
         else{ // Redraw held sharp key blue to show it is still held
             ctx.beginPath();
             ctx.fillStyle="blue";
             ctx.lineWidth = "1";
-            ctx.rect((position+0.75)*xCoordinate, 0, blackKeyWidth, blackKeyHeight);
+            ctx.rect((position+0.75)*xCoordinate, yCordinate, blackKeyWidth, blackKeyHeight);
             ctx.fill();            
         }
     }
@@ -150,36 +182,36 @@ function drawKeyboard(sharpNote){
 
 // Change the colour of a key when pressed
 function noteOnColour(octave,noteNumber,notePressed,sharpNote){
-    if(majorKeyPos.includes(notePressed)){
+    if (majorKeyPos.includes(notePressed)){
         ctx.beginPath();                
         ctx.fillStyle="red";
-        ctx.rect(((7*octave)+noteNumber)*xCoordinate, 0, whiteKeyWidth, whiteKeyHeight);
+        ctx.rect(((7*octave)+noteNumber)*xCoordinate, yCordinate, whiteKeyWidth, whiteKeyHeight);
         ctx.fill();
 
         drawKeyboard(sharpNote);
-    }else if(sharpKeyPos.includes(notePressed)){
+    } else if (sharpKeyPos.includes(notePressed)){
         let x = ((7*octave)+noteNumber)
         ctx.beginPath();                
         ctx.fillStyle="blue";
-        ctx.rect((x+0.75)*xCoordinate, 0, blackKeyWidth, blackKeyHeight);
+        ctx.rect((x+0.75)*xCoordinate, yCordinate, blackKeyWidth, blackKeyHeight);
         ctx.fill();
     }
 }
 
 // Change the colour of a key when released
 function noteOffColour(octave,noteNumber,notePressed,sharpNote){
-    if(majorKeyPos.includes(notePressed)){
+    if (majorKeyPos.includes(notePressed)){
         ctx.beginPath();                
         ctx.fillStyle="white";
-        ctx.rect(((7*octave)+noteNumber)*xCoordinate, 0, whiteKeyWidth, whiteKeyHeight);
+        ctx.rect(((7 * octave) + noteNumber) * xCoordinate, yCordinate, whiteKeyWidth, whiteKeyHeight);
         ctx.fill();
 
         drawKeyboard(sharpNote);
-    }else if(sharpKeyPos.includes(notePressed)){
+    } else if (sharpKeyPos.includes(notePressed)){
         let x = ((7*octave)+noteNumber)
         ctx.beginPath();                
         ctx.fillStyle="black";
-        ctx.rect((x+0.75)*xCoordinate, 0, blackKeyWidth, blackKeyHeight);
+        ctx.rect((x+0.75) * xCoordinate, yCordinate, blackKeyWidth, blackKeyHeight);
         ctx.fill();
 
     }
